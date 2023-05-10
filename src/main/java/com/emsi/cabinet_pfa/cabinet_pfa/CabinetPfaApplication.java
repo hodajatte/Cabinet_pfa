d50568deac0f234c56228e75cc6ecc1bcf8fda0a
hodajatte <88599603+hodajatte@users.noreply.github.com>
2023-05-10 13:33:27 +0100

    Initial commit

---
+++ src/main/java/com/emsi/cabinet_pfa/cabinet_pfa/CabinetPfaApplication.java
@@ -0,0 +1,13 @@
+package com.emsi.cabinet_pfa.cabinet_pfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetPfaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabinetPfaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emsi/cabinet_pfa/cabinet_pfa/CabinetPfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsi.cabinet_pfa.cabinet_pfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinetPfaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
